0ae7fef74003134ba75592c6691f8e93ef4d295a
Fizzzn <btordz6@gmail.com>
2025-01-29 20:11:59 +0000

    commit on main

--- src/Converter.java
+++ src/Converter.java
@@ -1,1 +1,28 @@
+public class converter{
 
+    public static void main(String [] args){
+     // Convert the string strFloat into a float.
+    String strFloat = "374.25";
+    float from_strFloat = (float) strFloat; // assign here
+
+    // Convert the string strInt into an integer.
+    String strInt = "1250";
+    int from_strInt = (int) strInt; // assign here
+
+    // Convert the float floatNum into an integer.
+    float floatNum = 56.715f;
+    int from_floatNum = (int) floatNumf; // assign here
+
+    // Convert the integer intNum into a float.
+    int intNum = 1500;
+    float from_intNum = (float) intnumf; // assign here
+
+    // Convert the double doubleNum into a string.
+    double doubleNum = 3.14159265;
+    String from_doubleNum = (String) doubleNum; // assign here
+                //done
+
+    }
+
+
+}
